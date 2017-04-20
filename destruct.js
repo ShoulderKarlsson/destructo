45c026dbe3afa1a89f029ec6724fb2366f02540c
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-04-20 23:09:55 +0200

    added destruct

---
+++ destruct.js
@@ -0,0 +1,31 @@
+function destruct(target, ...keys) {
+  const deepKeys = keys.filter(key => key.indexOf('.') > 1)
+  const deepObjects = deepKeys
+    .map(key => getDeepObject(target, key))
+    .reduce((build, obj) => Object.assign({}, build, obj), {})
+
+  const shallowObjects = keys.reduce((build, key) => {
+    return target.hasOwnProperty(key) ? 
+      Object.assign({}, build, { [key]: target[key] }) : build
+  }, {})
+
+  return Object.assign({}, shallowObjects, deepObjects)
+}
+
+function getDeepObject(target, key) {
+  if (target && target.hasOwnProperty(key)) {
+    return {[key]: target[key]}
+  }
+
+  const splittedKey = key.split('.')
+  const parent = splittedKey.shift()
+  const children = splittedKey.join('.')
+
+  if (target.hasOwnProperty(parent)) {
+    return getDeepObject(target[parent], children)
+  } else {
+    throw new Error(`Non existing key ${parent}`)
+  }
+}
+
+module.exports = destruct
